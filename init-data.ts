a199b640c906f0ea737a732b3cb23aea92cd0084
cedric-pouilleux <mino.mnz@gmail.com>
2024-05-07 23:34:05 +0200

    Fix populate:db

--- init-data.ts
+++ init-data.ts
@@ -10,70 +10,94 @@ if (!process.env.MONGO_DB_NAME) {
   throw new Error("MONGO_DB_NAME environment variable required");
 }
 
+const SlotSchemaFixtures = [
+  { name: "Tête" },
+  { name: "Cou" },
+  { name: "Epaule" },
+  { name: "Dos" },
+  { name: "Torse" },
+  { name: "Poignets" },
+  { name: "Mains" },
+  { name: "Taille" },
+  { name: "Jambes" },
+  { name: "Pieds" },
+  { name: "Doigt" },
+  { name: "Bijou" },
+  { name: "Relique" },
+  { name: "Main gauche" },
+  { name: "Main droite" },
+  { name: "Tenu en main gauche" },
+  { name: "A une main" },
+  { name: "Deux mains" },
+  { name: "A distance" },
+  { name: "Bouclier" },
+];
+
+const stuffGearSchemaFixtures = [
+  { name: "Bijou" },
+  { name: "Armures en tissu" },
+  { name: "Armures en cuir" },
+  { name: "Armures en plaques" },
+  { name: "Accessoire pour main gauche" },
+  { name: "Armures en mailles" },
+  { name: "Bouclier" },
+  { name: "Libram" },
+  { name: "Totem" },
+  { name: "Cape" },
+  { name: "Arbalète" },
+  { name: "Masse à une main" },
+  { name: "Masse à deux mains" },
+  { name: "Bâton" },
+  { name: "Dague" },
+  { name: "Épée à une main" },
+  { name: "Épée à deux mains" },
+  { name: "Hache à une main" },
+  { name: "Hache à deux mains" },
+  { name: "Arme de pugilat" },
+  { name: "Armes d'hast" },
+  { name: "Jeton" },
+  { name: "Quête" },
+  { name: "Anneau" },
+  { name: "Arme à feu" },
+  { name: "Arc" },
+  { name: "Schéma d'ingénierie" },
+];
+
 async function insertSlotsFixtures() {
-  await SlotSchema.create(
-    { name: "Tête" },
-    { name: "Cou" },
-    { name: "Epaule" },
-    { name: "Dos" },
-    { name: "Torse" },
-    { name: "Poignets" },
-    { name: "Mains" },
-    { name: "Taille" },
-    { name: "Jambes" },
-    { name: "Pieds" },
-    { name: "Doigt" },
-    { name: "Bijou" },
-    { name: "Relique" },
-    { name: "Main gauche" },
-    { name: "Main droite" },
-    { name: "Tenu en main gauche" },
-    { name: "A une main" },
-    { name: "Deux mains" },
-    { name: "A distance" },
-    { name: "Bouclier" }
-  );
+  try {
+    for await (const slot of SlotSchemaFixtures) {
+      console.log(slot);
+      await SlotSchema.findOneAndUpdate(slot, slot, {
+        upsert: true,
+        new: true,
+        setDefaultsOnInsert: true,
+      });
+    }
+  } catch (err: any) {
+    console.log(err);
+  }
 }
 
 async function insertStuffTypesFixtures() {
-  await StuffTypeSchema.create(
-    { name: "Bijou" },
-    { name: "Armures en tissu" },
-    { name: "Armures en cuir" },
-    { name: "Armures en plaques" },
-    { name: "Accessoire pour main gauche" },
-    { name: "Armures en mailles" },
-    { name: "Bouclier" },
-    { name: "Libram" },
-    { name: "Totem" },
-    { name: "Cape" },
-    { name: "Arbalète" },
-    { name: "Masse à une main" },
-    { name: "Masse à deux mains" },
-    { name: "Bâton" },
-    { name: "Dague" },
-    { name: "Épée à une main" },
-    { name: "Épée à deux mains" },
-    { name: "Hache à une main" },
-    { name: "Hache à deux mains" },
-    { name: "Arme de pugilat" },
-    { name: "Armes d'hast" },
-    { name: "Jeton" },
-    { name: "Quête" },
-    { name: "Anneau" },
-    { name: "Arme à feu" },
-    { name: "Arc" },
-    { name: "Schéma d'ingénierie" }
-  );
+  try {
+    for await (const gear of stuffGearSchemaFixtures) {
+      await StuffTypeSchema.findOneAndUpdate(gear, gear, {
+        upsert: true,
+        new: true,
+        setDefaultsOnInsert: true,
+      });
+    }
+  } catch (err: any) {
+    console.log(err);
+  }
 }
 
 async function insertItemsFixtures() {
-  console.log("inner insertItemsFixtures");
-  fs.readFile("./data.json", "utf-8")
+  await fs
+    .readFile("./data.json", "utf-8")
     .then(async (data) => {
-      console.log(data);
-      const items: any[] = JSON.parse(data);
-      for (const item of items) {
+      const items: any[] = await JSON.parse(data);
+      for await (const item of items) {
         const { gearType, slot } = item;
         const findSlot = slot.length
           ? await SlotSchema.findOne({ name: slot }).exec()
@@ -83,18 +107,27 @@ async function insertItemsFixtures() {
               name: gearType,
             }).exec()
           : undefined;
-        await ItemSchema.create({
-          ...item,
-          slot: findSlot?._id,
-          gearType: findGearType?._id,
-        });
+        const a = await ItemSchema.findOneAndUpdate(
+          { wowId: item.wowId },
+          {
+            ...item,
+            slot: findSlot?._id,
+            gearType: findGearType?._id,
+          },
+          {
+            upsert: true,
+            new: true,
+            setDefaultsOnInsert: true,
+          }
+        );
+        console.log(item);
       }
     })
     .catch((err: any) => {
-      Promise.reject(err);
+      return Promise.reject(err);
     })
     .finally(() => {
-      Promise.resolve();
+      return Promise.resolve();
     });
 }
 
@@ -109,6 +142,6 @@ async function insertItemsFixtures() {
   } catch (e) {
     console.log(e);
   } finally {
-    await mongoose.disconnect();
+    await mongoose.connection.close();
   }
 })();
